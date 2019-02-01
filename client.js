4c381196e806ea1064ba217c5899d1556089a64c
Katie Mangan <katie.mangan1@gmail.com>
2019-02-01 17:18:32 -0600

    collect input information on submit button click

--- client.js
+++ client.js
@@ -2,6 +2,20 @@ console.log('JS');
 
 $(document).ready(onReady);
 
-function onReady() {
-    console.log('document is ready!');
+function onReady() {   
+    $('#submitButton').on('click', submitButtonClick);
+
+
+
+
+
+
 }//end document ready
+
+function submitButtonClick(){
+   let firstName = ($('#inputFirstName').val()); 
+    let lastName = ($('#inputLastName').val());
+    let employeeID = ($('#inputID').val());
+    let employeeTitle = ($('#inputTitle').val());
+    let annualSalary = ($('#inputAnnualSalary').val());
+}
