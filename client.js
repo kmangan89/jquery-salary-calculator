844af105cd56c758499eb9001c3554af138d04a8
Katie Mangan <katie.mangan1@gmail.com>
2019-02-01 17:56:14 -0600

    added totalMonthlyCost to DOM

--- client.js
+++ client.js
@@ -2,20 +2,32 @@ console.log('JS');
 
 $(document).ready(onReady);
 
+let totalMonthlyCost = 0;
+
 function onReady() {   
     $('#submitButton').on('click', submitButtonClick);
-
-
-
-
-
-
+    $('#totalMonthlyCost').append(
+        '<h3>' + 'Total Monthly Cost: $'+ totalMonthlyCost + '</h3>'
+    );
 }//end document ready
 
 function submitButtonClick(){
-   let firstName = ($('#inputFirstName').val()); 
+    let firstName = ($('#inputFirstName').val()); 
     let lastName = ($('#inputLastName').val());
     let employeeID = ($('#inputID').val());
     let employeeTitle = ($('#inputTitle').val());
     let annualSalary = ($('#inputAnnualSalary').val());
-}
+    $('#employeeTableBody').append(`
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${employeeID}</td>
+            <td>${employeeTitle}</td>
+            <td>${annualSalary}</td>
+        </tr>    
+    `);
+
+    //clear inputs on click
+   console.log('hello');
+
+}// end submitButtonClick
